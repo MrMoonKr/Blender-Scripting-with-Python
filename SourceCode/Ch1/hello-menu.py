c152e200debefb982614508940a0fe9a33ecc7b7
Mr.Moon <MrMoonKr@gmail.com>
2025-08-02 16:26:27 +0900

    ...

--- SourceCode/Ch1/hello-menu.py
+++ SourceCode/Ch1/hello-menu.py
@@ -12,9 +12,9 @@ class SIMPLE_OT_print_hello( bpy.types.Operator ):
 
 
 # 2. 'Misc' 상위 메뉴 정의
-class VIEW3D_MT_misc_menu( bpy.types.Menu ):
+class CONSOLE_MT_misc_menu( bpy.types.Menu ):
     bl_label = "Misc"
-    bl_idname = "VIEW3D_MT_misc_menu"
+    bl_idname = "CONSOLE_MT_misc_menu"
 
     def draw(self, context):
         layout = self.layout
@@ -24,13 +24,13 @@ class VIEW3D_MT_misc_menu( bpy.types.Menu ):
 # 3. 상단 메뉴 바에 'Misc' 메뉴를 추가하는 함수
 def draw_misc_top_level( self, context ):
     layout: bpy.types.UILayout = self.layout
-    layout.menu( VIEW3D_MT_misc_menu.bl_idname, text="Misc", icon='PREFERENCES' )
+    layout.menu( CONSOLE_MT_misc_menu.bl_idname, text="Misc" )
 
 
 # 4. 등록 및 해제
 classes = (
     SIMPLE_OT_print_hello,
-    VIEW3D_MT_misc_menu,
+    CONSOLE_MT_misc_menu,
 )
 
 def register():
@@ -38,11 +38,13 @@ def register():
         bpy.utils.register_class( cls )
 
     # VIEW3D 상단 메뉴바에 Misc 추가
-    bpy.types.VIEW3D_MT_editor_menus.append( draw_misc_top_level )
+    # bpy.types.VIEW3D_MT_editor_menus.append( draw_misc_top_level )
+    bpy.types.CONSOLE_HT_header.append( draw_misc_top_level )
 
 def unregister():
     # VIEW3D 상단 메뉴바에서 Misc 제거
-    bpy.types.VIEW3D_MT_editor_menus.remove( draw_misc_top_level )
+    # bpy.types.VIEW3D_MT_editor_menus.remove( draw_misc_top_level )
+    bpy.types.CONSOLE_HT_header.remove( draw_misc_top_level )
 
     for cls in reversed(classes):
         bpy.utils.unregister_class(cls)
